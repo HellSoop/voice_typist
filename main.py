96bd0b2db0e5f1e21e747c0a28c57ec6b8c63761
HellSoop <igor.12.13.02.2006@gmail.com>
2024-06-27 11:46:50 +0500

    hot fix and several minor updates

--- main.py
+++ main.py
@@ -9,17 +9,18 @@ audio = pyaudio.PyAudio()
 CHUNK = 1000
 FORMAT = pyaudio.paFloat32
 SAMPLING_RATE = 16_000
+AUDIO_SCALE = 1.0
 
 
 def record_and_write():
-    stream = audio.open(format=FORMAT, channels=1, rate=SAMPLING_RATE, input=True)
     frames = []
+    stream = audio.open(format=FORMAT, channels=1, rate=SAMPLING_RATE, input=True)
 
     while keyboard.is_pressed('ctrl+alt'):
         frames.append(np.frombuffer(stream.read(CHUNK), dtype=np.float32))
 
     stream.close()
-    audio_data = np.concatenate(frames)
+    audio_data = np.concatenate(frames) * AUDIO_SCALE
 
     res = pipe(audio_data, generate_kwargs={"task": "transcribe"}, chunk_length_s=30, batch_size=8)
     keyboard.write(res['text'].strip())
--- starter.py
+++ starter.py
@@ -1,37 +1,52 @@
 import sys
 import subprocess as sp
 import keyboard
+from time import sleep
 from threading import Thread
 from pystray import Icon, Menu, MenuItem
 from PIL import Image
 
 
-def run_main():
+# model functions
+def run_main() -> None:
     process = sp.Popen([sys.executable, 'main.py'], stdout=sp.PIPE, stderr=sp.STDOUT, text=True)
     run_main.process = process
 
     for stdout_line in process.stdout:
-        if stdout_line in ('Model ready to work!\n', 'Model terminated\n'):
-            icon.notify(stdout_line)
-            if stdout_line == 'Model ready to work!\n':
+        stdout_line = stdout_line.strip()
+        if stdout_line in ('Model ready to work!', 'Model terminated'):
+            create_short_notification(stdout_line)
+            if stdout_line == 'Model ready to work!':
                 change_running_status.blocked = False
 
     process.stdout.close()
     run_main.process = None
 
 
-def start_main_thread():
+def start_main_thread() -> None:
     if not change_running_status.running:
         Thread(target=run_main, daemon=True).start()
         change_running_status(True)
-        icon.notify('Model is starting...')
+        create_short_notification('Model is starting...')
 
 
-def change_running_status(status: bool):
+def change_running_status(status: bool) -> None:
     if not change_running_status.blocked:
         change_running_status.running = status
         if status:
             change_running_status.blocked = True
+
+
+run_main.process = None
+change_running_status.blocked = False
+change_running_status.running = False
+
+
+# icon functions
+def create_short_notification(text: str) -> None:
+    icon.notify(text)
+    sleep(1.7)
+    icon.remove_notification()
 
 
 def close_app():
@@ -40,10 +55,7 @@ def close_app():
     icon.stop()
 
 
-run_main.process = None
-change_running_status.blocked = False
-change_running_status.running = False
-
+# create icon
 menu = Menu(
     MenuItem('Exit', close_app),
 )
